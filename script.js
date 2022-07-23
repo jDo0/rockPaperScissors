98b0af920219c5a9e18099f3fa422274fb9a26de
jDo0 <jackdolder1@gmail.com>
2022-07-23 09:41:36 +0100

    Connect randomNumber function to rock, paper, scissors outputs

--- script.js
+++ script.js
@@ -5,16 +5,28 @@ selectionButtons.forEach(selectionButton => {
         const selectionName = selectionButton.dataset.selection
         makeSelection(selectionName)
     })
-})
+});
 
 function makeSelection(selection) {
     console.log(selection)
-}
+};
 
 function randomNumber() {
     var randomNumber = Math.floor(Math.random()*3+1);
-    console.log(randomNumber);
-}
+    return randomNumber;
+};
 
-randomNumber()
+var RN = randomNumber()
 
+function RPS() {
+    if (RN==1){
+        return "rock"
+    }   else if (RN==2){
+        return "paper"
+    }   else if (RN==3){
+        return ("scissors")
+    }
+};
+var RPSTest = RPS();
+
+console.log(RPSTest);
